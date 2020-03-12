3891547e2de56dd1f108fe87795f0b76dde1214f
pjacobr <jacobp1794@gmail.com>
2020-03-12 06:51:53 +0000

    final udpate

--- assets/notification.js
+++ assets/notification.js
@@ -1,5 +1,5 @@
 function send_notification_mail({ first_name, last_name }) {
-  emailjs.init("user_Cb4HVKUDkk3CHjNvTKrAf");
+  emailjs.init("user_oojeeupPy9z7d45g2AGiI");
   emailjs.send("gmail", "customer_account_create_notification", {
     lastName: last_name,
     firstName: first_name
